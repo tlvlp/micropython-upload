a5da96aec0d09acae387d7657d66b29f73d98ebe
Peter Veres <tllvllp@gmail.com>
2019-08-16 01:52:50 +0200

    Added the forked version of ampy that includes PR#80 to update folder contents with the put method

--- dev/UPLOAD.sh
+++ dev/UPLOAD.sh
@@ -10,13 +10,13 @@ PROJECT_DIR=/home/tlvlp/projects/tlvlp-iot/mcu/tlvlp-iot-mcu-bazsalikon-soil
 ##################################
 
 # UPLOAD PROJECT
-#ampy --port ${PORT} --baud ${BAUD} -d ${DELAY} put ${PROJECT_DIR}/boot.py
-#ampy --port ${PORT} --baud ${BAUD} -d ${DELAY} put ${PROJECT_DIR}/main.py
-#ampy --port ${PORT} --baud ${BAUD} -d ${DELAY} put ${PROJECT_DIR}/hw hw
-#ampy --port ${PORT} --baud ${BAUD} -d ${DELAY} put ${PROJECT_DIR}/mqtt mqtt
-#ampy --port ${PORT} --baud ${BAUD} -d ${DELAY} put ${PROJECT_DIR}/uasyncio uasyncio
-#ampy --port ${PORT} --baud ${BAUD} -d ${DELAY} put ${PROJECT_DIR}/unit unit
-#ampy --port ${PORT} --baud ${BAUD} -d ${DELAY} put ${PROJECT_DIR}/wifi wifi
+#python3 ampy/cli.py --port ${PORT} --baud ${BAUD} -d ${DELAY} put ${PROJECT_DIR}/boot.py
+#python3 ampy/cli.py --port ${PORT} --baud ${BAUD} -d ${DELAY} put ${PROJECT_DIR}/main.py
+#python3 ampy/cli.py --port ${PORT} --baud ${BAUD} -d ${DELAY} put ${PROJECT_DIR}/hw hw
+#python3 ampy/cli.py --port ${PORT} --baud ${BAUD} -d ${DELAY} put ${PROJECT_DIR}/mqtt mqtt
+#python3 ampy/cli.py --port ${PORT} --baud ${BAUD} -d ${DELAY} put ${PROJECT_DIR}/uasyncio uasyncio
+python3 ampy/cli.py --port ${PORT} --baud ${BAUD} -d ${DELAY} put ${PROJECT_DIR}/unit unit
+#python3 ampy/cli.py --port ${PORT} --baud ${BAUD} -d ${DELAY} put ${PROJECT_DIR}/wifi wifi
 
 # UPLOAD FILE SEPARATELY
-#ampy --port ${PORT} --baud ${BAUD} -d ${DELAY} put ${PROJECT_DIR}/unit/config.py /unit/config.py
+#python3 ampy/cli.py --port ${PORT} --baud ${BAUD} -d ${DELAY} put ${PROJECT_DIR}/unit/config.py /unit/config.py
